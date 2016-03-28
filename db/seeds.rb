d15bd1bb51ae098ae18060724b60c00adb92c7e2
Sam Humphreys <s-humphreys@hotmail.com>
2016-03-29 10:45:21 +1100

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,8 @@
 User.destroy_all
-u1 = User.create :email => 'sam@ga.co', :password => 'chicken', :first_name => 'Sam', :surname => 'Humphreys', :social_media => 'https://twitter.com/iamasamwich', :is_admin => true
-u2 = User.create :email => 'amy@ga.co', :password => 'chicken', :first_name => 'Amy', :is_admin => false
+u1 = User.create :email => 'admin@ga.co', :password => 'chicken', :first_name => 'Sam', :surname => 'Humphreys', :social_media => 'https://twitter.com/iamasamwich', :is_admin => true
 
 Listing.destroy_all
-l1 = Listing.create :available => true, :address => '96/18 Cecilia Street, Marrickville'
-l2 = Listing.create :available => false, :address => '10/9 View Street, Marrickville'
 
 Review.destroy_all
-r1 = Review.create :review => 'Goooowooood'
 
-u1.reviews << r1
-u1.listings << l1
-u2.listings << l2
+Image.destroy_all
